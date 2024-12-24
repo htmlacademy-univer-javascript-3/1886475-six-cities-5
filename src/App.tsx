305b12029ed017a691ca2dc7b864e51775a9db25
na_slone <44521170+nolo7e@users.noreply.github.com>
2024-12-24 13:42:50 +0300

    first

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,27 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import {Login} from './pages/Login/Login.tsx';
 import {Favorites} from './pages/Favorites/Favorites.tsx';
 import {Offer} from './pages/Offer/Offer.tsx';
-import {NotFound} from './pages/NotFoundPage/NotFoundPage.tsx';
+import {NotFound} from './pages/NotFoundPage/NotFound.tsx';
 import {AppRoute} from './utils/const.ts';
 import {PrivateRoute} from './PrivateRoute.tsx';
 import {TPlaceCard} from './utils/types.ts';
+import {favorites} from './mocks/favorites.ts';
 
 interface IAppProps {
-  places: TPlaceCard[];
+  offers: TPlaceCard[];
 }
 
-export const App: React.FC<IAppProps> = ({places}): JSX.Element => {
+export const App: React.FC<IAppProps> = ({offers}): JSX.Element => {
   const isAuthenticated = false;
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={AppRoute.Main} element={<Main places={places} />} />
+        <Route path={AppRoute.Main} element={<Main places={offers} />} />
         <Route path={AppRoute.Login} element={<Login />} />
         <Route path={AppRoute.Favorites} element={
           <PrivateRoute isAuthenticated={isAuthenticated}>
-            <Favorites />
+            <Favorites places={favorites} />
           </PrivateRoute>
         }
         />
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {PlaceCard} from '../../components/PlaceCard/PlaceCard.tsx';
 import {TPlaceCard} from '../../utils/types.ts';
+import {OfferList} from '../../components/OfferList/OfferList.tsx';
 
 type MainProps = {
   places: TPlaceCard[];
@@ -36,6 +36,7 @@ export const Main: React.FC<MainProps> = ({places}) => (
         </div>
       </div>
     </header>
+
     <main className="page__main page__main--index">
       <h1 className="visually-hidden">Cities</h1>
       <div className="tabs">
@@ -78,7 +79,7 @@ export const Main: React.FC<MainProps> = ({places}) => (
         <div className="cities__places-container container">
           <section className="cities__places places">
             <h2 className="visually-hidden">Places</h2>
-            <b className="places__found">{places.length} places to stay in Amsterdam</b>
+            <b className="places__found">{places?.length} places to stay in Amsterdam</b>
             <form className="places__sorting" action="#" method="get">
               <span className="places__sorting-caption">Sort by</span>
               <span className="places__sorting-type" tabIndex={0}>
@@ -94,9 +95,7 @@ export const Main: React.FC<MainProps> = ({places}) => (
                 <li className="places__option" tabIndex={0}>Top rated first</li>
               </ul>
             </form>
-            <div className="cities__places-list places__list tabs__content">
-              { places.map((place) => (<PlaceCard key={place.name} place={place} />)) }
-            </div>
+            <OfferList offers={places}/>
           </section>
           <div className="cities__right-section">
             <section className="cities__map map"></section>
